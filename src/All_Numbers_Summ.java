cbc2d036152e71f29c1da8cd030060877349e1db
Vit <privit@rambler.ru>
2022-12-02 20:05:26 +0300

    Убрана лишняя переменная "n". Пережиток от прошлого задания.

--- src/All_Numbers_Summ.java
+++ src/All_Numbers_Summ.java
@@ -9,12 +9,10 @@ public class All_Numbers_Summ {
     public static void main(String[] args) {
 
         int [] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
-        int n = 0;
         for (int i = 0; i <= arr.length-2; i++) {
             for (int j = 1 + i; j <= arr.length-1; j++) {
                 if (arr[i] + arr[j] == -2) {
                     System.out.println(arr[i] + " + " + arr[j] + " = -2");
-                    n++;
                 }
             }
         }
